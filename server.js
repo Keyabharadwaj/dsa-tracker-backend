1511e362d46cc7ed9fee1cf352b4f03c03e41345
Keya Bhardwaj <keyabharadwaj2@gmail.com>
2026-04-06 03:32:58 +0530

    fix mongo final

--- server.js
+++ server.js
@@ -9,9 +9,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGOURI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log("Mongo Error:", err));
 
 app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
